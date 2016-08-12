7e3ed7115262daedac4a76a6b56e2e6633e3ebd2
Marcel Müller <neikos@neikos.email>
2016-08-12 13:50:34 +0200

    Add bootstrap from MaxCDN

--- src/views/layout/mod.rs
+++ src/views/layout/mod.rs
@@ -9,6 +9,8 @@ pub fn application(mut data: &mut fmt::Write, title: Cow<str>, partial: Cow<str>
         html {
             head {
                 title ^title
+                link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous" /
+                script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous" ""
             }
 
             body {
