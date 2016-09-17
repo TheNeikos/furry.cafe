36ca6db3264b3fe06c817009a3efc8826f0120c9
Marcel Müller <neikos@neikos.email>
2016-09-17 10:24:11 +0200

    Change error types to universal

--- src/error.rs
+++ src/error.rs
@@ -9,229 +9,49 @@ use std::error::Error;
 use std::fmt::{self, Debug};
 use models::user::User;
 
-#[derive(Debug)]
-pub struct NotImplemented {
-    route: String,
-}
-
-impl NotImplemented {
-    pub fn new(req: &Request) -> NotImplemented {
-        NotImplemented {
-            route: req.url.clone().into_generic_url().into_string()
+quick_error! {
+    #[derive(Debug)]
+    pub enum FurratoriaError {
+        NotImplemented(err: String) {
+            description(err)
         }
-    }
-}
-
-impl fmt::Display for NotImplemented {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for NotImplemented {
-    fn description(&self) -> &str { &self.route }
-}
-
-
-#[derive(Debug)]
-pub struct TemplateError {
-    cause: Option<Box<Error + Send>>,
-}
-
-impl TemplateError {
-    pub fn new() -> TemplateError {
-        TemplateError {
-            cause: None,
+        Template(err: Box<Error + Send>) {
+            cause(&**err)
+            description(err.description())
+            from()
         }
-    }
-}
-
-impl fmt::Display for TemplateError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for TemplateError {
-    fn description(&self) -> &str { "Template could not be parsed." }
-}
-
-impl From<fmt::Error> for TemplateError {
-    fn from(other: fmt::Error) -> Self {
-        TemplateError {
-            cause: Some(Box::new(other)),
+        Database(err: Box<Error + Send>) {
+            cause(&**err)
+            description(err.description())
+            from(diesel::result::Error)
         }
+        BadFormatting(err: Box<Error + Send>) {
+            cause(&**err)
+            description(err.description())
+            from(diesel::result::Error)
+        }
+        Login(err: Box<Error + Send>) {
+            cause(&**err)
+            description(err.description())
+            from(diesel::result::Error)
+            from(FurratoriaError::Internal)
+            from(FurratoriaError::Database)
+        }
+        Internal(err: Box<Error + Send>) {
+            cause(&**err)
+            description(err.description())
+            from(BcryptError)
+        }
+        Unauthorized(user: User) {
+            cause(&**err)
+            description(err.description())
+        }
+        NotFound {}
     }
 }
 
-impl From<TemplateError> for IronError {
-    fn from(e: TemplateError) -> IronError {
+impl From<FurratoriaError> for IronError {
+    fn from(e: FurratoriaError) -> IronError {
         IronError::new(Box::new(e), status::InternalServerError)
     }
 }
-
-#[derive(Debug)]
-pub struct DatabaseError {
-    cause: Option<Box<Error + Send>>,
-}
-
-impl DatabaseError {
-    pub fn new() -> DatabaseError {
-        DatabaseError {
-            cause: None,
-        }
-    }
-}
-
-impl fmt::Display for DatabaseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for DatabaseError {
-    fn description(&self) -> &str { "Database error." }
-}
-
-impl From<diesel::result::Error> for DatabaseError {
-    fn from(other: diesel::result::Error) -> Self {
-        DatabaseError {
-            cause: Some(Box::new(other)),
-        }
-    }
-}
-
-impl From<DatabaseError> for IronError {
-    fn from(e: DatabaseError) -> IronError {
-        IronError::new(Box::new(e), status::InternalServerError)
-    }
-}
-
-#[derive(Debug)]
-pub struct BadFormattingError {
-    cause: Option<Box<Error + Send>>,
-}
-
-impl BadFormattingError {
-    pub fn new() -> BadFormattingError {
-        BadFormattingError {
-            cause: None,
-        }
-    }
-}
-
-impl fmt::Display for BadFormattingError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for BadFormattingError {
-    fn description(&self) -> &str { "BadFormatting error." }
-}
-
-impl From<diesel::result::Error> for BadFormattingError {
-    fn from(other: diesel::result::Error) -> Self {
-        BadFormattingError {
-            cause: Some(Box::new(other)),
-        }
-    }
-}
-
-impl From<BadFormattingError> for IronError {
-    fn from(e: BadFormattingError) -> IronError {
-        IronError::new(Box::new(e), status::InternalServerError)
-    }
-}
-
-#[derive(Debug)]
-pub struct LoginError {
-    cause: Option<Box<Error + Send>>,
-}
-
-impl LoginError {
-    pub fn new() -> LoginError {
-        LoginError {
-            cause: None,
-        }
-    }
-}
-
-impl fmt::Display for LoginError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for LoginError {
-    fn description(&self) -> &str { "Login error." }
-}
-
-impl From<diesel::result::Error> for LoginError {
-    fn from(other: diesel::result::Error) -> Self {
-        LoginError {
-            cause: Some(Box::new(other)),
-        }
-    }
-}
-
-impl From<LoginError> for IronError {
-    fn from(e: LoginError) -> IronError {
-        IronError::new(Box::new(e), status::InternalServerError)
-    }
-}
-
-impl From<BcryptError> for LoginError {
-    fn from(other: BcryptError) -> Self {
-        error!("Could not run BCrypt: {:?}", other);
-        LoginError {
-            cause: None,
-        }
-    }
-}
-
-impl From<DatabaseError> for LoginError {
-    fn from(other: DatabaseError) -> Self {
-        LoginError {
-            cause: Some(Box::new(other)),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct UnauthorizedError {
-    pub user: Option<User>
-}
-
-impl UnauthorizedError {
-    pub fn new(u: Option<User>) -> UnauthorizedError {
-        UnauthorizedError {
-            user: u
-        }
-    }
-}
-
-impl fmt::Display for UnauthorizedError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for UnauthorizedError {
-    fn description(&self) -> &str { "Unauthorized" }
-}
-
-
-#[derive(Debug)]
-pub struct NotFound;
-
-impl fmt::Display for NotFound {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Debug::fmt(self, f)
-    }
-}
-
-impl Error for NotFound {
-    fn description(&self) -> &str { "Not Found" }
-}
-
--- src/macros.rs
+++ src/macros.rs
@@ -18,34 +18,6 @@ macro_rules! resource {
 }
 
 #[macro_export]
-macro_rules! template {
-    ($fun:expr) => {{
-        use error;
-        try!(match $fun {
-            ::std::result::Result::Ok(val) => ::std::result::Result::Ok(val),
-            ::std::result::Result::Err(err) => {
-                let e : error::TemplateError = ::std::convert::From::from(err);
-                ::std::result::Result::Err(e)
-            }
-        })
-    }}
-}
-
-#[macro_export]
-macro_rules! database_try {
-    ($fun:expr) => {{
-        use error;
-        match $fun {
-            ::std::result::Result::Ok(val) => ::std::result::Result::Ok(val),
-            ::std::result::Result::Err(err) => {
-                let e : error::DatabaseError = ::std::convert::From::from(err);
-                ::std::result::Result::Err(e)
-            }
-        }
-    }}
-}
-
-#[macro_export]
 macro_rules! temp_redirect {
     ($url:expr) => {
         (status::SeeOther, Redirect(url!($url)))
