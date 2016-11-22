ec51f70d62fcffb7e9911a3a401e1364be39e881
Marcel Müller <neikos@neikos.email>
2016-11-22 23:40:07 +0100

    Patch XSS attack in Markdown

--- src/views/markdown.rs
+++ src/views/markdown.rs
@@ -1,10 +1,11 @@
 use maud::{PreEscaped, RenderOnce};
 use maud_pulldown_cmark::Markdown;
-use pulldown_cmark::{Parser, Event};
+use pulldown_cmark::{Parser, Event, Tag};
 
 pub fn parse(s: &str) -> PreEscaped<String> {
     let events = Parser::new(s).map(|ev| match ev {
         Event::Html(html) | Event::InlineHtml(html) => Event::Text(html),
+        Event::Start(Tag::Link(ref d, ref t)) if d.starts_with("javascript:") => Event::Text(t.clone()),
         _ => ev
     });
 
